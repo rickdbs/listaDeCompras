bb20bc93f36a555a596a05090913bf0950f3bc84
Henrique de Brito <107428368+rickdbs@users.noreply.github.com>
2023-01-04 20:48:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/* Capture 10 itens para compor a lista de um supermercado.
+
+Após capturar os 10 items, imprima-os, separando por vírgula*/
+
+let itens = [];
+
+for(let item = 0; item < 10; item++) {
+    
+     let itemName = prompt = ("Digite o item " + (item + 1))
+
+     items[item] = itemName
+}
+
+alert(items)
